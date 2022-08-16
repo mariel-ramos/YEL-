bb80a0d4416197f96674d06c2597fca42068000d
mariel-ramos <111344331+mariel-ramos@users.noreply.github.com>
2022-08-16 16:08:34 +0800

    added file

---
+++ App.java
@@ -0,0 +1,22 @@
+import java.util.Queue;
+
+import.java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception{
+        
+        Queue<String> restaurantreservation = new LinkedList<>();
+        restaurantreservation.add("Chai");
+        restaurantreservation.add("Chai");
+        restaurantreservation.add("Chai");
+
+        System.out.println(Queue);
+
+        system
+    }
+}
+
+   
+
+    
+}
+
---
+++ stack.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class stack {
+
+    public static void main(String[] args) throws Exception{
+
+        Stack <String> stk = new Stack<>();
+
+    stk.push("white box");
+    stk.push("brown box");
+    stk.push("blue box");
+
+    stk.pop();
+    System.out.println(stk);
+    }
+    
+    
+
+}
